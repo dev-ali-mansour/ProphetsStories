f38d4f6c47210dfe2b463247577b45853a48fd0d
Ali Mansour <dev.ali.mansour@gmail.com>
2025-07-12 19:41:15 +0300

    build: Migrate project level build.gradle to Kotlin DSL
    This commit migrates the top-level `build.gradle` file from Groovy to Kotlin DSL (`build.gradle.kts`).
    
    The classpath dependencies previously defined in the `buildscript` block are now managed as plugins within the `plugins` block, referencing aliases from the version catalog (`libs`).
    
    A workaround `println("")` has been added to address a potential "Expecting an expression" build error in the Kotlin DSL file.

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.kotlin.dsl) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.kotlin.ksp) apply false
+    alias(libs.plugins.google.services) apply false
+}
+//Workaround for "Expecting an expression" build error
+println("")
